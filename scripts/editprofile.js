a08fb6f586d7f416b5e757545a8b65c39c931d5f
ChaseDelgadillo <omgitschasehunter@gmail.com>
2017-04-10 22:42:16 -0700

    Add files via upload

--- scripts/editprofile.js
+++ scripts/editprofile.js
@@ -1,6 +1,6 @@
 var EDIT_SUBMIT_BUTTON_SELECTOR = '[data-button="edit-submit"]';
 var USERNAME_SELECTOR = 'newusername';
-var KARMA_SELECTOR = 'karma';
+var KARMA_SELECTOR = 'bio';
 
 function consoleLog(someMessage) { console.log(someMessage); }
 
@@ -18,8 +18,14 @@ function createButtonEvent() {
     $(button).click(function() {
         consoleLog("Right before AJAX");
         var newusername = document.getElementById("newusername").value;
-        var karma = document.getElementById("karma").value;
-        consoleLog("New Thread Created");
+        var bio = document.getElementById("bio").value;
+        consoleLog(newusername);
+        if (newusername.length == 0) {
+            newusername = "null entry";
+        }
+        if (bio.length == 0) {
+            bio = "null entry";
+        }
         if (newusername.length < 4) {
             alert("New username length too short. 4+ characters.");
         } else {
@@ -28,7 +34,7 @@ function createButtonEvent() {
                 type: "POST",
                 data: {
                     newusernameEntered: newusername,
-                    karmaEntered: karma
+                    bioEntered: bio
                 },
                 success: function(result) {
                     consoleLog("Inside success function.");
